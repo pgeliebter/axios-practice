6ae21c57983a6754d496450fcfad88fde2f0135c
Pinny Geliebter <pgibralter@gmail.com>
2021-10-10 16:16:31 -0400

    init commit

---
+++ axios.js
@@ -0,0 +1,19 @@
+import "regenerator-runtime/runtime";
+import axios from "axios";
+
+// old code
+// const getRequest = (url, paramsObject) => {
+//   axios
+//     .get(url, paramsObject)
+//     .then((response) => {
+//       console.log(`GET from ${url}`, response.data);
+//     })
+//     .catch((error) => console.error(error));
+// };
+
+// let gitHubRequest = {
+//   url: "https://api.github.com/repos/vuejs/vue",
+//   paramsObject: {},
+// };
+
+// getRequest(gitHubRequest.url);
